644be1f27b2945a9236e122a2136a1b761fff463
taciturn02 <taciturn248@gmail.com>
2024-04-06 16:26:26 +0530

    Login Page made successfully

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import React ,{lazy} from 'react'
+import {BrowserRouter,Route,Routes} from "react-router-dom"
+
+const Home = lazy(()=> import("./pages/Home"));
+const Login = lazy(()=> import("./pages/Login"));
+const Chat = lazy(()=> import("./pages/Chat"));
+const Groups = lazy(()=> import("./pages/Groups"));
+
+
+const App = () => {
+  return (
+    <BrowserRouter>
+
+        <Routes>
+          <Route path = "/" element={<Home />}/>
+          <Route path = "/chat/:chatId" element={<Chat/>}/>
+          <Route path = "/Groups" element={<Groups/>}/>
+          <Route path = "/login" element={<Login/>}/>
+        </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
