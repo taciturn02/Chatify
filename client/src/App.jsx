98b32fbb07143c0b3ec7794d0d40d8769da198b9
taciturn02 <taciturn248@gmail.com>
2024-04-07 18:08:42 +0530

    Header of Pages and Sign up page created successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,22 +1,44 @@
-import React ,{lazy} from 'react'
+import React ,{Suspense, lazy} from 'react'
 import {BrowserRouter,Route,Routes} from "react-router-dom"
+import ProtectRoute from './components/auth/ProtectRoute';
+import { LayoutLoader } from './components/layout/Loaders';
+
 
 const Home = lazy(()=> import("./pages/Home"));
 const Login = lazy(()=> import("./pages/Login"));
 const Chat = lazy(()=> import("./pages/Chat"));
 const Groups = lazy(()=> import("./pages/Groups"));
+const NotFound = lazy(()=> import("./pages/NotFound"));
 
+let user = true;
 
 const App = () => {
   return (
     <BrowserRouter>
+       <Suspense fallback = {<LayoutLoader />}>
 
-        <Routes>
-          <Route path = "/" element={<Home />}/>
-          <Route path = "/chat/:chatId" element={<Chat/>}/>
-          <Route path = "/Groups" element={<Groups/>}/>
-          <Route path = "/login" element={<Login/>}/>
-        </Routes>
+       <Routes>
+
+      <Route
+        element={<ProtectRoute user={user} redirect="/login"/>} >
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+      </Route>
+
+      <Route
+        path="/login"
+        element={
+        <ProtectRoute user={!user} redirect="/">
+          <Login />
+        </ProtectRoute>
+      }
+    />
+
+    <Route path='*' element = {< NotFound/>}></Route>
+
+      </Routes>
+       </Suspense>
 
     </BrowserRouter>
   )
