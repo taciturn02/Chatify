18e8f25cc5679b2792c70d19c89bc20b87e6e08c
taciturn02 <taciturn248@gmail.com>
2024-04-12 17:43:15 +0530

    ChatItem and ChatList component created and included in AppLayout successfully

---
+++ client/src/constants/sampleData.js
@@ -0,0 +1,17 @@
+export const samepleChats = [
+    {
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      name: "shivam",
+      _id: "1",
+      groupChat: false,
+      members: ["2", "3"],
+    },
+  
+    {
+      avatar: ["https://www.w3schools.com/howto/img_avatar.png"],
+      name: "Raj",
+      _id: "2",
+      groupChat: true,
+      members: ["1", "2"],
+    },
+  ];
